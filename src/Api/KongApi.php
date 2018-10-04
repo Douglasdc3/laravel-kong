4bb7554ff5aece43aed12b41e4465a77465ae69d
Oleh Kravets <xpoback@gmail.com>
2018-10-04 10:56:03 +0200

    Use constant for status code

--- src/Api/KongApi.php
+++ src/Api/KongApi.php
@@ -6,6 +6,8 @@ use DouglasDC3\Kong\Kong;
 
 abstract class KongApi
 {
+    const STATUS_NO_CONTENT = 204;
+
     /**
      * @var \DouglasDC3\Kong\Http\HttpClient
      */
@@ -85,7 +87,7 @@ abstract class KongApi
      */
     protected function deleteCall($url)
     {
-        return $this->kong->getClient()->delete($url)->getStatusCode() === 204;
+        return $this->kong->getClient()->delete($url)->getStatusCode() === self::STATUS_NO_CONTENT;
     }
 
     /**
